23182fa6a3e7d71fc2d9e48f6063b557eb612bc3
Caleb Gibson <caleb@Calebs-MacBook-Pro.local>
2024-11-10 15:27:09 -0500

    Made some styling changes

--- photography-website/src/components/SplashPage.vue
+++ photography-website/src/components/SplashPage.vue
@@ -2,15 +2,19 @@
   <div class="flex flex-col justify-center w-full space-y-3">
     <div class="flex w-full justify-center relative">
       <div
-        class="flex w-1/2 z-1 text-black font-semibold text-left pl-4 text-3xl text-pretty"
+        class="absolute flex w-1/2 bottom-4 left-1 text-white font-semibold text-3xl text-pretty"
         name="large-heading"
       >
         Hello! My Name is Caleb Gibson and this is my Photography Website!
       </div>
-      <img class="flex justify-center w-full" src="../assets/Me-wide-2.png" />
+      <img
+        class="flex justify-center space-y-0 w-full"
+        alt="Caleb Gibson"
+        src="../assets/Me-wide-2.png"
+      />
     </div>
     <div
-      class="flex font-normal self-center text-medium w-3/4"
+      class="flex font-normal self-center text-xl w-3/4"
       name="about-details"
     >
       I have been interested in Photography for about 5 years now, and mostly
